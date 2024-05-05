7458516a88a2cb84bd62873f52315ad2f384a203
W Cardoso <cardosu7@gmail.com>
2024-05-05 07:59:12 -0300

    pesquisa usando set

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Maria", 99217896);
+        agendaContatos.adicionarContato("João", 99265896);
+        agendaContatos.adicionarContato("Pedro", 98214587);
+        agendaContatos.adicionarContato("Marcia", 97852145);
+        agendaContatos.adicionarContato("Monica", 98521235);
+
+        agendaContatos.exibirContatos();
+        System.out.println("---------------------------------");
+        System.out.println(agendaContatos.pesquisarPorNome("Maria"));
+        System.out.println("---------------------------------");
+        System.out.println(agendaContatos.atualizarNumeroContato("Maria", 555555));
+        System.out.println("---------------------------------");
+        agendaContatos.exibirContatos();
+        System.out.println("---------------------------------");
+
+
+
     }
 }
